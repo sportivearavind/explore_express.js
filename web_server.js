7f67e6f86073349eceacac06ec1e5e3f1c6836d7
Aravind <aravind16101800@gmail.com>
2024-06-14 22:22:07 +0530

    feat: handling route for both / & index.html

--- web_server.js
+++ web_server.js
@@ -1,12 +1,17 @@
 // We will convert a computer to server using express.js in this file
-const express = require('express')
-const app = express()  // app is widely used naming convension
-const path = require('path')
-const PORT = process.env.PORT || 3500;
+const express = require('express') //imports the Express module, which is a web framework for Node.js. It simplifies the process of building web applications
 
-//get the file from the client
-app.get('/', (req,res) =>{
-    res.send('Hi Full Stack Developer')
+const app = express()  // instance of the Express application
+const path = require('path') //imports the Path module, which provides utilities for working with file and directory paths. It is part of Node.js's core modules.
+
+const PORT = process.env.PORT || 3500; //This line sets the port number on which the server will listen for incoming requests
+
+//get the file from the client and send the response
+//app.get() is a method to define a route handler for GET requests.
+app.get('^/$|/index.html', (req,res) =>{
+  // sends the index.html file located in the views directory as the response.
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 })
 
+// The callback function () => console.log(Server running on port ${PORT}) is executed once the server starts
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
